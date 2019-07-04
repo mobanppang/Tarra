f889d6e478502908ce602cbd7f5a09372e2e7cf7
mobanppang <51999488+mobanppang@users.noreply.github.com>
2019-07-05 00:03:24 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,19 @@
+var imgs=2
+var now=0
+
+function slide(){
+    now=(now==imgs)?0:now+=1
+    $('.slide li').eq(now).animate({
+        marginLeft:'0'
+    })
+    $('.slide li').eq(now-1).animate({
+        marginLeft:'-1920px'
+    })
+}
+
+function start(){
+    $('.slide li').eq(0).siblings().animate({
+        marginLeft:'-1920px'
+    })
+    setInterval(slide,4000)
+}start()
